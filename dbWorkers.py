4e30421bdd1cb199acea658d135f0d20c0075d31
saikatgomes <saikat@cs.wisc.com>
2014-09-09 15:40:06 -0500

    get(k) works

--- dbWorkers.py
+++ dbWorkers.py
@@ -8,26 +8,28 @@ myCursor = None
 dbName="allData"
 dbTable="allData" #messedupSomewhere!
 
-def hello(someValue):
-	print "Hello MaderC."
-	print("your value is "+someValue)
-	return 'abc'
-
 def startConnection():
 	global myCursor, myConnection
 	print "Creating Connection ..."
 	#myConnection = sqlite3.connect('db/allData')
         myConnection = sqlite3.connect('db/'+dbName)
 	myCursor = myConnection.cursor()
+	#TODO: check if connection was successful
 
 def stopConnection():
 	global myCursor, myConnection
 	myConnection.close()
         myConnection = None
         myCursor = None
+	#TODO: check if successful
 
 def get(key):
-	value=key
+	global myCursor, myConnection
+        #check if connection is open else open it!
+        if not myCursor:
+                startConnection()
+        myCursor.execute("SELECT value from "+dbTable+" where key = '"+key+"'")
+        value=myCursor.fetchone()[0]
 	return value
 
 def put(key,value):
@@ -37,10 +39,17 @@ def put(key,value):
 		startConnection()
 	myCursor.execute("INSERT INTO "+dbTable+" VALUES ('"+key+"','"+value+"')")
 	myConnection.commit()
+	#TODO: check if successful
 
 if __name__ == '__main__':
-	hello("saikat")
 	startConnection();	
 	ts = time.time()
 	put(datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'),"newValue")
+	value=get("1")
+	print("Value for Key=1 is "+value)
 	stopConnection();
+	#testing when connection is closed!
+        value=get("2")
+        print("Value for Key=2 is "+value)
+	stopConnection();
+
