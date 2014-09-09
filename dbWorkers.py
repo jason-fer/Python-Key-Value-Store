d7e95fca65efefb9c64f487bbd41fa0f549e935c
saikatgomes <saikat@cs.wisc.com>
2014-09-09 14:28:14 -0500

    created start and stop db methods

--- dbWorkers.py
+++ dbWorkers.py
@@ -1,4 +1,26 @@
 import sqlite3
-myConnection = sqlite3.connect('db/allData')
-myCursor = myConnection.curson()
+#myConnection = sqlite3.connect('db/allData')
+#myCursor = myConnection.cursor()
 
+def hello(someValue):
+	print "Hello MaderC."
+	print("your value is "+someValue)
+	return 'abc'
+
+def startConnection():
+	myConnection = sqlite3.connect('db/allData')
+	myCursor = myConnection.cursor()
+
+def stopConnection():
+	myConnection.close()
+
+def get(key):
+	#do db connection here
+	return value
+
+def put(key,value):
+	#do db connection here
+	pass
+
+if __name__ == '__main__':
+	hello("saikat")
