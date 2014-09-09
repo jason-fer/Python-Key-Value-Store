431de2352aaca4cd477d5f24205cc220e3f2b00e
Jason Feriante <jason.feriante@gmail.com>
2014-09-09 14:32:53 -0500

    Added stub methods for get / create key-values

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+
+def get_value():
+  # key = request.args.get('key');
+  # return key
+  return "get_value()"
+  #run Saikats get method to retrieve the value from the db
+  #just return the result
+
+def create_value():
+  return "create_value()"
+  #json decode the payload
+  #make sure there's at least a key
+  #run Saikats create method to set the value in the database
+  #just return the result
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+  if request.method == 'GET':
+      return get_value()
+  elif  request.method == 'POST':
+      return create_value()
+  else:
+      return 'error'
+
+if __name__ == "__main__":
+  app.debug = True
+  app.run()
+
