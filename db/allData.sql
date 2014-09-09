f4c308e2d845490a4163bdc92cd2b954a9e7b3bf
saikatgomes <saikat@cs.wisc.com>
2014-09-09 13:49:40 -0500

    Adding Database

--- db/allData.sql
+++ db/allData.sql
@@ -1,7 +1,5 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE allData(key varchar(128), value varchar(2048));
-INSERT INTO "allData" VALUES('1','Saikat');
-INSERT INTO "allData" VALUES('2','Rahul');
-INSERT INTO "allData" VALUES('3','Jason');
+CREATE INDEX keyIndex on allData (key);
 COMMIT;
---
+++ db/allData.sql.old
@@ -0,0 +1,7 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE allData(key varchar(128), value varchar(2048));
+INSERT INTO "allData" VALUES('1','Saikat');
+INSERT INTO "allData" VALUES('2','Rahul');
+INSERT INTO "allData" VALUES('3','Jason');
+COMMIT;
