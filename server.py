6e9ffdd5127c0aabf9f69a8d4165e661431e0a80
Jason Feriante <jason.feriante@gmail.com>
2014-09-09 15:23:39 -0500

    spaces to tabs

--- server.py
+++ server.py
@@ -9,36 +9,36 @@ app = Flask(__name__)
 dbWorkers.startConnection()
 
 def get_value():
-  key = request.args.get('key');
+	key = request.args.get('key');
 
-  return dbWorkers.get(key)
+	return dbWorkers.get(key)
 
 def create_value():
-  key = request.form['key'];
-  value = request.form['value'];
+	key = request.form['key'];
+	value = request.form['value'];
 
-  if not key or not value: 
-    return 'error'
+	if not key or not value: 
+		return 'error'
 
-  return dbWorkers.put(key, value)
+	return dbWorkers.put(key, value)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-  if request.method == 'GET':
-    key = request.args.get('key', 'WTFFF');
-    print key
-    return json.dumps({'key': key, 'value':get_value()})
-  elif  request.method == 'POST':
-    key = request.form.get('key', '')
-    value = request.form.get('value', '')
-    print key, value
-    return json.dumps({'key': key, 'value':value})
-  else:
-      return 'error'
+	if request.method == 'GET':
+		key = request.args.get('key', 'WTFFF');
+		print key
+		return json.dumps({'key': key, 'value':get_value()})
+	elif  request.method == 'POST':
+		key = request.form.get('key', '')
+		value = request.form.get('value', '')
+		print key, value
+		return json.dumps({'key': key, 'value':value})
+	else:
+			return 'error'
 
 if __name__ == "__main__":
-  app.debug = True
-  app.run()
+	app.debug = True
+	app.run()
 
 
 
