d9bd10aea09caa42ae34f2281bcde35163dc6ae1
saikatgomes <saikat@cs.wisc.com>
2014-09-12 14:14:46 -0500

    fixed - rahul

--- dbWorkers.py
+++ dbWorkers.py
@@ -12,45 +12,57 @@ def startConnection():
 	global myCursor, myConnection
 	print "Creating Connection ..."
 	#myConnection = sqlite3.connect('db/allData')
-        myConnection = sqlite3.connect('db/'+dbName)
+	myConnection = sqlite3.connect('db/'+dbName)
 	myCursor = myConnection.cursor()
 	#TODO: check if connection was successful
 
 def stopConnection():
 	global myCursor, myConnection
 	myConnection.close()
-        myConnection = None
-        myCursor = None
+	myConnection = None
+	myCursor = None
 	#TODO: check if successful
 
 def get(key):
 	global myCursor, myConnection
-        #check if connection is open else open it!
-        if not myCursor:
-                startConnection()
-        myCursor.execute("SELECT value from "+dbTable+" where key = '"+key+"'")
-        value=myCursor.fetchone()[0]
-	return value
-
-def put(key,value):
-        global myCursor, myConnection
 	#check if connection is open else open it!
 	if not myCursor:
 		startConnection()
+
+	# throws an error if you try to retrieve a key that doesn't exist
+	try:
+		myCursor.execute("SELECT value from "+dbTable+" where key = '"+key+"'")
+		value=myCursor.fetchone()[0]
+	except:
+		value = ''
+		
+	return value
+
+def put(key,value):
+	global myCursor, myConnection
+	#check if connection is open else open it!
+	if not myCursor:
+		startConnection()
+
 	myCursor.execute("INSERT INTO "+dbTable+" VALUES ('"+key+"','"+value+"')")
 	myConnection.commit()
+
 	#TODO: check if successful
 
-if __name__ == '__main__':
+def unit_test():
 	#UNIT TEST BELOW!
 	startConnection();	
 	ts = time.time()
 	put(datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'),"newValue")
+	put("1", "hello")
+	put("2", "world")
 	value=get("1")
 	print("Value for Key=1 is "+value)
 	stopConnection();
 	#testing when connection is closed!
-        value=get("2")
-        print("Value for Key=2 is "+value)
+	value=get("2")
+	print("Value for Key=2 is "+value)
 	stopConnection();
 
+if __name__ == '__main__':
+	unit_test()
--- server.py
+++ server.py
@@ -7,41 +7,36 @@ put_in_db = dbWorkers.put
 
 app = Flask(__name__)
 
-#@todo: json encode / decode payload
-
-dbWorkers.startConnection()
-
 def get_value():
 	key = request.args.get('key', '');
-	return dbWorkers.get(key)
+	return {'key': key, 'value': get_from_db(key)}
 
 def create_value():
-	key = request.form['key'];
-	value = request.form['value'];
+	key = request.form.get('key', '')
+	value = request.form.get('value', '')
 
 	if not key or not value: 
-		return json.dumps({'status': 'error', 'message': 'get() requires a key'})
+		return {'status': 'error', 'message': 'get() requires a key'}
 
-	return dbWorkers.put(key, value)
+	old_value = get_from_db(key)
+
+	data = {'return': -1, 
+				'key': key,
+				'value': value,
+				'old_value': old_value}
+
+	data['return'] = 0 if old_value else 1
+
+	put_in_db(key, value)
+
+	return data;
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
 	if request.method == 'GET':
-		key = request.args.get('key', '');
-		return json.dumps({'key': key, 'value': get_from_db()})
-
+		return json.dumps(get_value()) 
 	elif  request.method == 'POST':
-		key = request.form.get('key', '')
-		value = request.form.get('value', '')
-		d  = {'return': -1, 
-					'value': "",
-					'old_value': ""}
-		if not key or not value:
-			return d
-		o_val = get_from_db(key)
-		d['return'] = 0 if o_val else 1
-		ret = put_in_db(key, value)
-		return json.dumps(d)
+		return json.dumps(create_value())
 	else:
 		return json.dumps({'status': 'error', 'message': 'only GET or POST are valid methods'})
 
