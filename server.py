3fe7834c027c8b2a7df2b3cbefff38c3c57333c5
Jason Feriante <jason.feriante@gmail.com>
2014-09-12 19:18:37 -0500

    key & value lengths are now checked

--- server.py
+++ server.py
@@ -12,35 +12,36 @@ app = Flask(__name__)
 def get_value():
 	key = request.args.get('key', '');
 	status, value = get_from_db(key);
-	# assume present
 
-	return  {
-						'return': status,
-						'key': key,
-						'value': value
-					}
+	# throw error if there's no key
+	if not key : 
+		return  {'return': status, 'errors': ['get() requires a key']}
+
+	# throw error if key or value exceed length limits
+	if len(key.encode('utf-8')) > 128:
+		return  { 'return': status, 'errors': ['key length must not exceed 128 bytes'] }
+
+	return  {'return': status, 'key': key, 'value': value }
 
 def create_value():
 	key = request.form.get('key', '')
 	value = request.form.get('value', '')
 	status = -1
 
+	# throw error if key or value not present
 	if not key or not value: 
-		return  {
-							'return': status,
-							'errors': [
-								'get() requires a key'
-							]
-						}
+		return  { 'return': status, 'errors': ['put() requires a key and a value'] }
+
+	# throw error if key or value exceed length limits
+	if len(key.encode('utf-8')) > 128:
+		return  { 'return': status, 'errors': ['key length must not exceed 128 bytes'] }
+
+	if len(value.encode('utf-8')) > 2048:	
+		return  { 'return': status, 'errors': ['value length must not exceed 2048 bytes'] }
 
 	status, old_value = put_in_db(key, value);
 
-	data = {
-					'return': status, 
-					'key': key,
-					'value': value,
-					'old_value': old_value
-				}
+	data = {'return': status, 'key': key, 'value': value, 'old_value': old_value}
 
 	return data;
 
