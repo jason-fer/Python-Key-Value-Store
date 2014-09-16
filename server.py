1f4872486cf3c0b768733ca57376f962201730b8
Jason Feriante <jason.feriante@gmail.com>
2014-09-16 13:58:35 -0500

    server update

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, json
 import dbWorkers
 
+# 500: error
+# 404: key doesn't exist
+# 200: key exists
+# 201: key created 
+
 get_from_db = dbWorkers.get
 put_in_db = dbWorkers.put
 app = Flask(__name__)
@@ -8,12 +13,12 @@ app = Flask(__name__)
 def check_key(method_name, key):
 	# throw error if there's no key
 	if not key: 
-		return  False, {'return': -1, 'errors': [ method_name + ' requires a key']}
+		return  False, {'errors': [ method_name + ' requires a key']}
 
 	# throw error if key exceeds length limits
 	key_length = len(key.encode('utf-8'))
 	if key_length > 128:
-		return  False, { 'return': -1, 'errors': [ method_name + ' key was ' 
+		return  False, {'errors': [ method_name + ' key was ' 
 		+  str(key_length) + ' bytes. This exceeds the 128 byte limit'] }
 
 	# the key is OK
@@ -22,12 +27,12 @@ def check_key(method_name, key):
 def check_value(method_name, value):
 	# throw error value not present
 	if not value: 
-		return  False, { 'return': -1, 'errors': [ method_name + ' requires a value'] }
+		return  False, {'errors': [ method_name + ' requires a value'] }
 
 	# throw error if value exceeds length limits
 	value_length = len(value.encode('utf-8'))
 	if value_length > 2048:	
-		return  False, { 'return': -1, 'errors': [ method_name + ' value was ' 
+		return  False, {'errors': [ method_name + ' value was ' 
 		+ str(value_length) + ' bytes. This exceeds the 2048 byte limit'] }
 
 	# the value is OK
@@ -39,12 +44,21 @@ def get_value():
 	#check the key
 	ok, error_message = check_key('get()', key);
 	if not ok:
-		return error_message
+		# 500 = error
+		return error_message, 500
 
 	# retrieve the value
 	status, value = get_from_db(key);
 
-	return  {'return': status, 'key': key, 'value': value }
+	# key wasn't present = 404 not found
+	if status == 1:
+		return  {'value': '', 'errors': [ 'key not found'] }, 404
+	# system error = 500 server error
+	elif status == -1:
+		return  {'value': value, 'errors': [ 'unknown database error'] }, 500
+	# key was present = 200 ok
+	else:
+		return  {'value': value }, 200
 
 def put_value():
 	key = request.form.get('key', '')
@@ -53,29 +67,37 @@ def put_value():
 	#check the key
 	ok, error_message = check_key('put()', key);
 	if not ok:
-		return error_message
+		return error_message, 500
 
 	#check the value
 	ok, error_message = check_value('put()', value);
 	if not ok:
-		return error_message
+		return error_message, 500
 
 	status, old_value = put_in_db(key, value);
 
-	data = {'return': status, 'key': key, 'value': value, 'old_value': old_value}
+	# key wasn't present = 201 created (insert)
+	if status == 1:
+		return  {'value': value}, 201
+	# system error = 500 server error
+	elif status == -1: 
+		return  {'value': value, 'errors': [ 'unknown database error'] }, 500
+	# key was present = 200 ok (update)
+	else:
+		return  {'value': value, 'old_value': old_value}, 200
 
-	return data;
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'PUT'])
 def main():
 	if request.method == 'GET':
-		return json.dumps(get_value()) 
+		data, http_status = get_value() 
+		return json.dumps(data), http_status
 
-	elif  request.method == 'POST':
-		return json.dumps(put_value())
+	elif request.method == 'PUT':
+		data, http_status = put_value()
+		return json.dumps(data), http_status
 
 	else:
-		return json.dumps({'status': 'error', 'message': 'GET and POST are the only allowed HTTP methods'})
+		return json.dumps({'errors':['Not implemented']}), 404
 
 if __name__ == "__main__":
 	app.debug = True
