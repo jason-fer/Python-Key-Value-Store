efe218ec50cfbc03f62ae7e7f7ade34d4fe1a7de
saikatgomes <saikat@cs.wisc.com>
2014-09-16 17:07:03 -0500

    Adding logging functionality

--- dbWorkers.py
+++ dbWorkers.py
@@ -8,6 +8,7 @@ clientIP=""
 myConnection = None
 myCursor = None
 msgType = ["INFO","ERROR"]
+logFileObj = None
 
 dbName = "allData"
 dbTable = "allData"
@@ -19,25 +20,56 @@ def getTime():
     dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d|%H:%M:%S|%f')
     return dt
 
+def getDate():
+    ts = time.time()
+    dt = datetime.datetime.fromtimestamp(ts).strftime('%d%m%Y')
+    return dt
+
 # print std message to console
 def msg(type,method,message,IP=None):
+    global logFileObj
     if not IP:
         IP="<client_unknown>"
-    print(getTime()+"|"+msgType[type]+"|"+str(os.getpid())+"|"+IP+"|"+method+"|"+message) 
+    dt=getDate()
+    logParentDir='log'
+    logFile=logParentDir+'/'+dt+".log"
+    if not os.path.isdir(logParentDir):
+        print(getTime()+"|"+msgType[0]+"|"+str(os.getpid())+"|"+IP+"|"+"LOGGING|creating parent log dir")
+        try:
+            os.makedirs(logParentDir)
+            print(getTime()+"|"+msgType[0]+"|"+str(os.getpid())+"|"+IP+"|"+"LOGGING|parent log folder created: "+logParentDir)
+        except:
+            print(getTime()+"|"+msgType[1]+"|"+str(os.getpid())+"|"+IP+"|"+"LOGGING|parent log  folder could not be created at "+logParentDir)
+    if not logFileObj:
+        if not os.path.exists(logFile):
+            print(getTime()+"|"+msgType[0]+"|"+str(os.getpid())+"|"+IP+"|"+"LOGGING|creating daily log file: "+logFile)
+            try:
+                logFileObj=open(logFile,"a")
+                print(getTime()+"|"+msgType[0]+"|"+str(os.getpid())+"|"+IP+"|"+"LOGGING|daily log file created: "+logFile)
+            except:
+                print(getTime()+"|"+msgType[1]+"|"+str(os.getpid())+"|"+IP+"|"+"LOGGING|daily log file "+logFile+" cannot be created!!!")
+    try:
+        logFileObj=open(logFile,"a")
+    except:
+        print(getTime()+"|"+msgType[1]+"|"+str(os.getpid())+"|"+IP+"|"+"LOGGING|daily log file "+logFile+" cannot be accessed")
+    logMsg=getTime()+"|"+msgType[type]+"|"+str(os.getpid())+"|"+IP+"|"+method+"|"+message
+    if logFileObj:
+        logFileObj.write(logMsg+"\n")
+    print(logMsg)
 
 # intiate DB connection
 # returns 1 for success and 0 for failure
 def startConnection(IP=None):
     global myCursor, myConnection
     isSuccess = 0;
-    msg(0,"START_DB","Creating connection to " + dbName + "." + dbTable)
+    msg(0,"START_DB","Creating connection to " + dbName + "." + dbTable,IP)
     try:
         myConnection = sqlite3.connect('db/' + dbName)
         myCursor = myConnection.cursor()
         isSuccess = 1
-        msg(0,"START_DB","Successful connected to " + dbName + "." + dbTable)
+        msg(0,"START_DB","Successful connected to " + dbName + "." + dbTable,IP)
     except:
-        msg(1,"START_DB","Failed to connected to " + dbName + "." + dbTable)
+        msg(1,"START_DB","Failed to connected to " + dbName + "." + dbTable,IP)
 
     return isSuccess
 
@@ -45,18 +77,21 @@ def startConnection(IP=None):
 # stopping DB connection
 # returns 1 for success and 0 for failure
 def stopConnection(IP=None):
-    global myCursor, myConnection
+    global myCursor, myConnection,logFileObj
     opType="STOP_DB"
     isClosed = 0;
-    msg(0,opType,"Closing connection to " + dbName + "." + dbTable)
+    msg(0,opType,"Closing connection to " + dbName + "." + dbTable,IP)
     try:
         myConnection.close()
         myConnection = None
         myCursor = None
         isClosed = 1
-        msg(0,opType,"Connection closed to " + dbName + "." + dbTable)
+        msg(0,opType,"Connection closed to " + dbName + "." + dbTable,IP)
     except:
-        msg(1,opType,"Failed to close connection to " + dbName + "." + dbTable)
+        msg(1,opType,"Failed to close connection to " + dbName + "." + dbTable,IP)
+    if logFileObj:
+        logFileObj.close()
+        logFileObj=None
     return isClosed
 
 
@@ -71,7 +106,7 @@ def get(key,IP=None):
     retFlag = 0
     opType="GET"
     if not myCursor:
-        startConnection()
+        startConnection(IP)
     value = ''
     try:
         myCursor.execute("SELECT value from " + dbTable + " where key = '" + key + "'")
@@ -79,14 +114,14 @@ def get(key,IP=None):
         if d:
             value = d[0]
             retFlag = 0
-            msg(0,opType,"key='" + key + "' found!")
+            msg(0,opType,"key='" + key + "' found!",IP)
         else:
             retFlag = 1
-            msg(0,opType,"key='" + key + "' not found!")
+            msg(0,opType,"key='" + key + "' not found!",IP)
     except:
         retFlag = -1
         print sys.exec_info()
-        msg(1,opType,"operation failed for key='" + key + "'!")
+        msg(1,opType,"operation failed for key='" + key + "'!",IP)
 
     return retFlag, value
 
@@ -112,7 +147,7 @@ def getAll(IP=None):
         for row in myCursor.execute("SELECT * FROM " + dbTable):
             allData.append((row[0],row[1]))
             count=count+1
-        msg(0,opType,str(count)+" row(s) fetched")
+        msg(0,opType,str(count)+" row(s) fetched",IP)
         if count==0:
             retFlag=1
         else:
@@ -136,20 +171,20 @@ def put(key, value, IP=None):
     if not myCursor:
         startConnection()
     try:
-        msg(0,opType,"adding key='" + key + "' & value='"+value+"'")
-        retFlag, oldV = get(key)
+        msg(0,opType,"adding key='" + key + "' & value='"+value+"'",IP)
+        retFlag, oldV = get(key,IP)
         if retFlag == -1:
             raise
         elif retFlag == 1:
             myCursor.execute("INSERT INTO " + dbTable + " VALUES ('" + key + "','" + value + "')")
-            msg(0,opType,"value='"+value+"' insert!")
+            msg(0,opType,"value='"+value+"' insert!",IP)
         else:
             myCursor.execute("UPDATE " + dbTable + " SET value='" + value + "' where key='" + key + "'")
-            msg(0,opType,"value updated from '"+oldV+"' to '"+value+"'")
+            msg(0,opType,"value updated from '"+oldV+"' to '"+value+"'",IP)
         myConnection.commit()
-        msg(0,opType,"successful")
+        msg(0,opType,"successful",IP)
     except:
-        msg(1,opType,"failed!")
+        msg(1,opType,"failed!",IP)
 
     return retFlag, oldV
 
@@ -167,66 +202,67 @@ def delete(key, IP=None):
     if not myCursor:
         startConnection()
     try:
-        msg(0,opType,"deleting key='" + key + "'")
-        retFlag, oldV = get(key)
+        msg(0,opType,"deleting key='" + key + "'",IP)
+        retFlag, oldV = get(key,IP)
         if retFlag == -1:
             raise
         elif retFlag == 0:
             myCursor.execute("DELETE FROM " + dbTable + " WHERE key='" + key + "'")
-            msg(0,opType,"value='"+oldV+"' deleted!")
+            msg(0,opType,"value='"+oldV+"' deleted!",IP)
         else:
-            msg(0,opType,"key to be deleted not found.")
+            msg(0,opType,"key to be deleted not found.",IP)
         myConnection.commit()
-        msg(0,opType,"successful")
+        msg(0,opType,"successful",IP)
     except:
-        msg(1,opType,"failed!")
+        msg(1,opType,"failed!",IP)
 
     return retFlag, oldV
 
 def unit_test():
     #UNIT TEST BELOW!
+    myIP="localhost:unit_test"
     print ""
     print "*****      TEST 0 START: starting connection"
-    startConnection();
+    startConnection(myIP);
     print "*****      TEST 0 END"
     testKey = getTime()
     print ""
     print "*****      TEST 1 START: adding an new value"
-    res, oldV = put(testKey, "newValue")
+    res, oldV = put(testKey, "newValue",myIP)
     print "*****      OUTPUT: ", res
     print "*****      TEST 1 END"
     print ""
     print "*****      TEST 2 START: adding an old value"
-    res, oldV = put(testKey, "newValue")
+    res, oldV = put(testKey, "newValue",myIP)
     print "*****      OUTPUT: ", res
     print "*****      TEST 2 END"
     print ""
     print "*****      TEST 3 START: get an value that exist"
-    res, value = get("1")
+    res, value = get("1",myIP)
     print("*****      Value for Key=1 is " + value)
     print "*****      OUTPUT: ", res
     print "*****      TEST 3 END"
     print ""
     print "*****      TEST 4 START: get an value that does not exist"
-    res, value = get("IdontExist")
+    res, value = get("IdontExist",myIP)
     print("*****      Value for Key=IdontExist is " + value)
     print "*****      OUTPUT: ", res
     print "*****      TEST 4 END"
     print ""
     print "*****      TEST 5 START: delete a value that does not exist"
-    res, value = delete("IdontExist")
+    res, value = delete("IdontExist",myIP)
     print("*****      Old Value for Key=IdontExist is " + value)
     print "*****      OUTPUT: ", res
     print "*****      TEST 5 END"
     print ""    
     print "*****      TEST 6 START: delete a value that exists"
-    res, value = delete(testKey)
+    res, value = delete(testKey,myIP)
     print("*****      Old Value for Key="+testKey+" is " + value)
     print "*****      OUTPUT: ", res
     print "*****      TEST 6 END"
     print ""  
     print "*****      TEST 8 START: get all (key,value)"
-    res, count, allData = getAll()
+    res, count, allData = getAll(myIP)
     localCount=0
     for row in allData:
         localCount=localCount+1
@@ -235,14 +271,14 @@ def unit_test():
     print "*****      TEST 8 END"
     print ""
     print "*****      TEST 8 START: close connection"
-    stopConnection();
+    stopConnection(myIP);
     print "*****      TEST 8 END"
     print ""
     print "*****      TEST 9 START: get an value that exist with connection closed!"
-    res, value = get("1")
+    res, value = get("1",myIP)
     print("*****      Value for Key=1 is " + value)
     print "*****      OUTPUT: ", res
-    stopConnection();
+    stopConnection(myIP);
     print "*****      TEST 9 END"
     print ""
 
