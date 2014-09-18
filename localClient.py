02ceec0cd41eef8422ed58e29668dd771468dcde
saikatgomes <saikat@cs.wisc.com>
2014-09-18 18:48:59 -0500

    engine to run local client with interface <as before>

---
+++ localClient.py
@@ -0,0 +1,60 @@
+import urllib2
+import json
+import os, sys
+import urllib
+import requests 
+from config import *
+import client
+
+url = None
+def UI(args):
+	global url
+	if len(args)>1:
+		client.get_url(args[1])
+	else:
+		client.get_url()
+	print url
+	con = client.kv739_init(url)
+	if con==-1:
+		print "Connection refused!"
+		exit(0);
+	else:
+		print "Connection successful!"
+
+
+	while(1):
+		cmd = raw_input("cmd: [G]et/[P]ut/[Q]uit/[D]elete: ")
+		if cmd.upper() == 'G':
+			key = raw_input( "Key: " )
+			print "Key:", key 
+			r, val = client.kv739_get(key)
+			print "Code:", r
+			print "Value:", val
+		elif cmd.upper() == 'P':
+			key = raw_input( "Key: " )
+			print "Values:",
+			value = sys.stdin.readline().strip()
+			ret, o_val = client.kv739_put(key, value)
+			if ret not in [0,1,-1]:
+				print "Wrong return value:", ret
+			elif ret == 0:
+				print "Updated Key\nKey:", key 
+				print "Old Value:", o_val
+				print "New Value:", value
+			else:
+				print "Inserted Key\nKey:", key 
+				print "New Value:", value
+		elif cmd.upper() == 'D':
+			key = raw_input( "Key: " )
+			print "Key:", key 
+			ret, o_val = client.kv739_delete(key)
+			# delete the shit
+			print "Code:", ret
+			if ret == 200:
+				print "O_Val:", o_val
+		elif cmd.upper() == 'Q':
+			print 
+			exit(0)
+
+if __name__ == "__main__":
+	UI(sys.argv)
