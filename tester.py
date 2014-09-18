18eeb96d0eccd34db0d9f02cac73dbddc2d48fe5
saikatgomes <saikat@cs.wisc.com>
2014-09-18 15:44:52 -0500

    run mutiple test and create *.perf files

--- tester.py
+++ tester.py
@@ -4,13 +4,13 @@ import random
 import time
 from config import *
 import string
+import cProfile
 
 
 def get_rand_string(n=128):
     return ''.join(random.choice(string.ascii_uppercase + \
                                  string.ascii_lowercase + \
                                  string.digits + ' ') for _ in range(n))
-    
 
 
 def put_test(n=10000, n_key=128, n_val=128):
@@ -20,15 +20,15 @@ def put_test(n=10000, n_key=128, n_val=128):
         if r==-1:
             print "SERVER CRASHED CRASHED!!!!!"
             time.sleep(1)
-        print i, r, o_val
+        i#print i, r, o_val
 
-def get_test(n=10000):
+def get_test(n=10000, n_key=128, n_val=128):
     for i in xrange(n):
         key, value = get_rand_string(n_key), get_rand_string(n_val)
         r, val = client.kv739_get(key, value)
         print r, val
 
-def del_test(n=10000):
+def del_test(n=10000, n_key=128, n_val=128):
     for i in xrange(n):
         key, value = get_rand_string(n_key), get_rand_string(n_val)
         r, val = client.kv739_delete(key, value)
@@ -41,7 +41,14 @@ def load_test(args):
 
 if __name__ == "__main__":
     client.kv739_init(sys.argv[1])
-    put_test(100)
-    #UI(sys.argv)
-		
-	
+    call = ['put']
+    num = [10]
+    k_size = [100]
+    v_size = [100]
+    for n in num:
+        for k in k_size:
+             for v in v_size:
+                 for t in call:
+                     testName=t+"_test("+str(n)+","+str(k)+","+str(v)+")"
+                     testFileName=t+"_n-"+str(n)+"_k-"+str(k)+"_v-"+str(v)+".perf"
+                     cProfile.run(testName,testFileName)
