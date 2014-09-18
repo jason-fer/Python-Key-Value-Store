1e4f581d71def8dbb270e654dd609c7d4ae166d3
saikatgomes <saikat@cs.wisc.com>
2014-09-18 18:34:51 -0500

    per test configurable to test client or server

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ Configuration flies for serveral parameters
 CACHE_VALUES = True
 MAX_CACHE_SIZE = 1024 #1024
 
-LOGGING_OFF = True
-PRINT_TO_SCREEN = False
+LOGGING_OFF = False 
+PRINT_TO_SCREEN = True
 DBNAME = "allData"
 DBTABLE = "allData"
 
--- tests/perf_run.py
+++ tests/perf_run.py
@@ -1,12 +1,27 @@
 import os,sys, timeit
 sys.path.insert(0,'../')
 import client
+import dbWorkers
 import random
 import time
 from config import *
 import string
 import cProfile
 import logger
+import timeit
+
+PERF_TEST_SERVER=True
+
+if PERF_TEST_SERVER:
+    get=dbWorkers.get
+    put=dbWorkers.put
+    delete=dbWorkers.delete
+    init=dbWorkers.startConnection
+else:
+     get=client.kv739_get
+     put=client.kv739_put
+     delete=client.kv739_delete
+     init=client.kv739_init
 
 msg=logger.msg
 getTime=logger.getTime
@@ -25,23 +40,23 @@ def get_rand_string(n=128):
 def put_test(n=10000, n_key=128, n_val=128):
     for i in xrange(n):
         key, value = get_rand_string(n_key), get_rand_string(n_val)
-        r, o_val = client.kv739_put(key, value)
+        r, o_val = put(key, value)
         if r==-1:
             print "SERVER CRASHED CRASHED!!!!!"
             time.sleep(1)
-        i#print i, r, o_val
+        #print i, r, o_val
 
 def get_test(n=10000, n_key=128, n_val=128):
     for i in xrange(n):
         key, value = get_rand_string(n_key), get_rand_string(n_val)
-        r, val = client.kv739_get(key, value)
-        print r, val
+        r, val = get(key)
+        #print r, val
 
-def del_test(n=10000, n_key=128, n_val=128):
+def delete_test(n=10000, n_key=128, n_val=128):
     for i in xrange(n):
         key, value = get_rand_string(n_key), get_rand_string(n_val)
-        r, val = client.kv739_delete(key, value)
-        print r, val
+        r, val = delete(key)
+        #print r, val
 
 
 def load_test(args):
@@ -49,7 +64,9 @@ def load_test(args):
     pass
 
 if __name__ == "__main__":
-    client.kv739_init(sys.argv[1])
+    serverUrl=sys.argv[1]
+    #client.kv739_init(serverUrl)
+    init(serverUrl)
     logParentDir = 'log'
     if not os.path.isdir(logParentDir):
         print(getTime() + "|INFO|" + str(os.getpid()) + "|" + myIP + "|" + opType + "|creating log dir")
@@ -59,19 +76,23 @@ if __name__ == "__main__":
         except:
             print(getTime() + "|ERROR|" + str(os.getpid()) + "|" + myIP + "|" + opType + "|log  folder could not be created at " + logParentDir)
 
-    call = ['put']
-    num = [10]
-    k_size = [100]
-    v_size = [100]
+    call = ['get','put','delete']
+    num = [1]
+    k_size = [10]
+    v_size = [10]
+    msgAppend="["+serverUrl+"] "
     for n in num:
         for k in k_size:
              for v in v_size:
                  for t in call:
                      try:
                          testName=t+"_test("+str(n)+","+str(k)+","+str(v)+")"
-                         testFileName=logParentDir+"/"+t+"_n-"+str(n)+"_k-"+str(k)+"_v-"+str(v)+".perf"
-                         msg(0, opType, "Starting test: "+testName, myIP, "test")
-                         cProfile.run(testName,testFileName)
-                         msg(0, opType, "Test completed: "+testName, myIP, "test")
+                         testFileName=logParentDir+"/"+t+"_n-"+str(n)+"_k-"+str(k)+"_v-"+str(v)+"__"+getTime()+".perf"
+                         testFileName=logParentDir+"/"+t+"_n-"+str(n)+"_k-"+str(k)+"_v-"+str(v)+".perf" 
+                         msg(0, opType, msgAppend+"Starting test: "+testName, myIP, "test")
+                         #cProfile.run(testName) #,testFileName)
+                         t = timeit.timeit(stmt=testName, number=1, setup='from __main__ import get_test, put_test, delete_test')
+			 msg(0, opType, msgAppend+"Test completed: "+testName, myIP, "test")
+                         msg(0, opType, msgAppend+"Time taken: "+testName+": " + str(t), myIP, "test")
                      except:
-                         msg(1, opType, "Unexcepted failure with test: "+testName, myIP, "test")
+                         msg(1, opType, msgAppend+"Unexcepted failure with test: "+testName, myIP, "test")
